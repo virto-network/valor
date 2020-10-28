9e1b60c4eed4ddff4ec0e8cc014f5fd990814c2c
Daniel Olano <daniel@olanod.com>
2020-10-28 08:17:49 +0100

    Native and Web packages

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn connect(url: &str) {
-    log(url);
+pub fn handle_req() {
+    println!("Handling request");
 }
---
+++ valor-web/src/lib.rs
@@ -0,0 +1,13 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen(start)]
+pub fn run() {
+    log("Hola!");
+    valor::handle_req();
+}
