e48e8e113a0a93bfeb1aa18a088254ed12cda08a
Daniel Olano <daniel@olanod.com>
2021-01-19 17:28:16 +0100

    Strip URL prefix when handing request to plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-//! Valor
+//! ## Valor
+//!
+//! A lightweight HTTP plugin system that runs in the server and the browser.
+//!
+//! - Use `valor_bin` to run your Rust and JS(soon!) plugins in the server.
+//! - Use `valor_web` as a script imported from the main document or a worker
+//! in your web application to have a local API powered by a service worker.
 
 // short-hand for creating or modifiying simple responses
 macro_rules! res {
@@ -36,15 +42,20 @@ pub type Request = http_types::Request;
 pub type Response = http_types::Response;
 type Result = core::result::Result<Response, Response>;
 
-/// Handler is the main entry point for dispatching incoming requests
-/// to registered plugins under a specific URL pattern.
+/// The main entry point for dispatching incoming requests
+/// to plugins registered under a specific URL prefix.
 ///
 /// ```
-/// # use http_types::{StatusCode, Request};
-/// let handler = Handler::new();
-/// let request = Request::new();
-/// let res = handler.handle_request(request).await?;
-/// assert_eq(res, StatusCode::Ok);
+/// # use valor_core::*;
+/// # #[async_std::main] async fn main() { test().await }
+/// # async fn test() {
+/// let handler = Handler::new(()).with_plugin("foo", ());
+///
+/// let request = Request::new(Method::Get, "http://example.com/foo/123");
+/// let res = handler.handle_request(request).await;
+///
+/// assert_eq!(res.status(), StatusCode::Ok);
+/// # }
 /// ```
 pub struct Handler<L> {
     registry: Rc<RefCell<PluginRegistry>>,
@@ -60,6 +71,7 @@ impl<L: Loader + 'static> Handler<L> {
         }
     }
 
+    /// Adds a plugin with its handler to the internal registry
     pub fn with_plugin<H>(&self, plugin: impl Into<Plugin>, handler: H)
     where
         H: RequestHandler + 'static,
@@ -69,27 +81,35 @@ impl<L: Loader + 'static> Handler<L> {
             .register(plugin.into(), Box::new(handler));
     }
 
+    /// Using the configured loader, loads a plugin to
     pub async fn load_plugin(&self, plugin: Plugin) -> core::result::Result<(), LoadError> {
         let handler = self.loader.load(&plugin).await?;
         self.with_plugin(plugin, handler);
         Ok(())
     }
 
+    /// Expose the plugin registry as an endpoint on `_plugins` to add more plugins dynamically
     pub fn with_registry(self) -> Self {
         self.with_plugin(
-            BuiltInPlugin::Registry,
+            Plugin::Static {
+                name: "registry".into(),
+                prefix: Some("_plugins".into()),
+            },
             PluginRegistry::as_handler(self.registry.clone(), self.loader.clone()),
         );
         self
     }
 
+    /// Include the built-in health plugin that returns _Ok_ on `_health`
     pub fn with_health(self) -> Self {
-        self.with_plugin(BuiltInPlugin::Health, |_| async { res!() });
+        self.with_plugin("health", ());
         self
     }
 
-    /// Handle the incoming request and send back a response
-    /// from the matched plugin to the caller.
+    /// Handles an incoming request by answering form a plugin that matches the URL pattern
+    ///
+    /// It requires the request to specify a `x-request-id` header that is set back on
+    /// the response as `x-correlation-id`(e.g. used by valor_web to match requests and responses)
     pub async fn handle_request(&self, request: impl Into<Request>) -> Result {
         let request = request.into();
         let req_id = request
@@ -120,10 +140,12 @@ impl<L> Clone for Handler<L> {
     }
 }
 
-/// Loader
+/// A Loader can fetch plugin handlers from various sources
+/// such as the network or the file system
 #[async_trait(?Send)]
 pub trait Loader: 'static {
     type Handler: RequestHandler;
+
     /// Loads the given `plugin`
     async fn load(&self, plugin: &Plugin) -> LoadResult<Self>;
 }
@@ -137,7 +159,16 @@ pub enum LoadError {
     BadFormat,
 }
 
-/// Request handler
+/// A dummy loader
+#[async_trait(?Send)]
+impl Loader for () {
+    type Handler = ();
+    async fn load(&self, _plugin: &Plugin) -> LoadResult<Self> {
+        Ok(())
+    }
+}
+
+/// Request handlers only job is to respond to http requests
 #[async_trait(?Send)]
 pub trait RequestHandler {
     /// Handles the request
@@ -155,13 +186,25 @@ where
     }
 }
 
+/// A dummy handler
+#[async_trait(?Send)]
+impl RequestHandler for () {
+    /// Handles the request
+    async fn handle_request(&self, _request: Request) -> Response {
+        StatusCode::Ok.into()
+    }
+}
+
 /// Plugin information
 #[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Plugin {
-    /// Built in
-    BuiltIn(BuiltInPlugin),
-    /// Native
+    /// Plugin that comes with the runtime
+    Static {
+        name: String,
+        prefix: Option<String>,
+    },
+    /// Natively compiled Rust plugin
     Native {
         /// Name
         name: String,
@@ -186,49 +229,40 @@ pub enum Plugin {
 
 impl Plugin {
     fn name(&self) -> &str {
-        match self {
-            Self::BuiltIn(p) => p.name(),
+        &match self {
+            Self::Static { name, .. } => name,
             Self::Native { name, .. } => name,
             Self::Web { name, .. } => name,
         }
-        .into()
     }
 
     fn prefix(&self) -> &str {
         match self {
-            Self::BuiltIn(p) => p.prefix(),
-            Self::Native { name, prefix, .. } => prefix.as_ref().unwrap_or(name),
-            Self::Web { name, prefix, .. } => prefix.as_ref().unwrap_or(name),
+            Self::Static { prefix, .. } => prefix,
+            Self::Native { prefix, .. } => prefix,
+            Self::Web { prefix, .. } => prefix,
+        }
+        .as_ref()
+        .map(|p| p.as_str())
+        .unwrap_or(self.name())
+        .trim_matches(&['/', ' '][..])
+    }
+}
+
+impl From<&str> for Plugin {
+    fn from(name: &str) -> Self {
+        Plugin::Static {
+            name: name.into(),
+            prefix: Some(format!("_{}", name)),
         }
     }
 }
 
-impl From<BuiltInPlugin> for Plugin {
-    fn from(p: BuiltInPlugin) -> Self {
-        Self::BuiltIn(p)
-    }
-}
-
-/// Plugins included with the runtime
-#[derive(Debug, Clone, PartialEq, Hash, Eq, Serialize, Deserialize)]
-#[serde(tag = "name", rename_all = "snake_case")]
-pub enum BuiltInPlugin {
-    Registry,
-    Health,
-}
-
-impl BuiltInPlugin {
-    fn name(&self) -> &str {
-        match self {
-            Self::Registry => "plugin_registry",
-            Self::Health => "health",
-        }
-    }
-
-    fn prefix(&self) -> &str {
-        match self {
-            Self::Registry => "_plugins",
-            Self::Health => "_health",
+impl From<(&str, &str)> for Plugin {
+    fn from((name, prefix): (&str, &str)) -> Self {
+        Plugin::Static {
+            name: name.into(),
+            prefix: Some(prefix.into()),
         }
     }
 }
