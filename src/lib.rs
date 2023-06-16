103887293a3a24e534705485cdf30941c883861a
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2023-06-16 02:46:48 -0500

    chore(valor): expose inner crates on main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
-pub fn on_message() {}
+#![feature(error_in_core)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use valor_core::{map, module, primitives};
+pub use valor_proc::{extensions, method, module};
+
+#[cfg(target_arch = "wasm32")]
+pub use valor_core::interop;
