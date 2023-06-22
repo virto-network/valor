07f9bf5a1eb511a10307e928eab9fafed1f42d8a
Johan Alexis Duque Cadena <johan.duque@gmail.com>
2023-06-22 15:05:58 +0200

    Including libwallet in Valor Embedded

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,118 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::rng::Rng;
+use embassy_nrf::{bind_interrupts, peripherals, rng, uarte};
 use embassy_time::{Duration, Timer};
+use heapless::String;
+use rand::Rng as _;
 use {defmt_rtt as _, panic_probe as _};
 
-use wasm_runtime::{Runtime, Wasm};
+bind_interrupts!(struct Irqs {
+    UARTE0_UART0 => uarte::InterruptHandler<peripherals::UARTE0>;
+    RNG => rng::InterruptHandler<peripherals::RNG>;
+});
+
+// Libwallet usage
+// use libwallet::{self, vault};
+// type Wallet = libwallet::Wallet<vault::Simple>;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
+    let mut rng = Rng::new(p.RNG, Irqs);
+    let mut config = uarte::Config::default();
+    config.parity = uarte::Parity::EXCLUDED;
+    config.baudrate = uarte::Baudrate::BAUD115200;
 
+    let mut uart = uarte::Uarte::new(p.UARTE0, Irqs, p.P0_08, p.P0_06, config);
+
+    info!("uarte initialized!");
+
+    // Async API
+    let mut bytes = [0; 4];
+    rng.fill_bytes(&mut bytes).await;
+    defmt::info!("Some random bytes: {:?}", bytes);
+
+    // Message must be in SRAM
+    // let mut buf: [u8; 80] = [0u8; 80];
+    let mut palabra: String<80> = String::new();
+
+    // Limpio pantalla y me pongo al inicio
+    let _ = palabra.push(0x1B as char);
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    let _ = palabra.push_str("[2J");
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    let _ = palabra.push(0x1B as char);
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    let _ = palabra.push_str("[H");
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    palabra.clear();
+
+    // Lanzo banner
+    let _ = palabra.push_str("*********************************************\r\n");
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    palabra.clear();
+    let _ = palabra.push_str("   Usando bindgen y contando por la UART!\r\n");
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    palabra.clear();
+    let _ = palabra.push_str("*********************************************\r\n\n");
+    unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+    palabra.clear();
+
+    // let phrase: &str = "";
+
+    // let vault = vault::Simple::generate(&mut rng);
+
+    // let mut wallet = Wallet::new(vault);
+    // wallet.unlock(None).await;
+    // let account = wallet.default_account();
+
+    // Hago conteo
+    // let mut conteo: u8 = 0;
     loop {
-        led.set_high();
-        Timer::after(Duration::from_millis(300)).await;
-        led.set_low();
-        Timer::after(Duration::from_millis(300)).await;
+        // info!("reading...");
+        // unwrap!(uart.read(&mut buf).await);
+        info!("writing...");
+        let _ = palabra.push_str("Conteo: ");
+        let random_num = rng.gen_range(0..9);
+        info!("random_num: {}", random_num);
+        let _ = palabra.push((49u8 + (random_num as u8)) as char);
+        let _ = palabra.push_str("\r\n");
+        // conteo = (conteo + 1) % 9;
+        unwrap!(uart.write(&palabra.clone().into_bytes()).await);
+        palabra.clear();
+        Timer::after(Duration::from_millis(1000)).await;
     }
+    // info!("wrote hello in uart!");
 }
+
+/* ********************************************************/
+// #![no_std]
+// #![no_main]
+// #![feature(type_alias_impl_trait)]
+
+// use defmt::*;
+// use embassy_executor::Spawner;
+// use embassy_nrf::gpio::{Level, Output, OutputDrive};
+// use embassy_time::{Duration, Timer};
+// use {defmt_rtt as _, panic_probe as _};
+
+// // use wasm_runtime::{Runtime, Wasm};
+
+// #[embassy_executor::main]
+// async fn main(_spawner: Spawner) {
+//     let p = embassy_nrf::init(Default::default());
+//     let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
+
+//     loop {
+//         led.set_high();
+//         Timer::after(Duration::from_millis(300)).await;
+//         led.set_low();
+//         Timer::after(Duration::from_millis(300)).await;
+//     }
+// }
+/**********************************************************/
 
 // mod constants;
 // mod parsero;
